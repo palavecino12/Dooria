e05af95aa914ec529f15bf14e062bc1b03a319df
Facundo <facundopalavecino054@gmail.com>
2026-04-18 19:18:06 -0300

    Ahora anda la funcion de editar en el componente MonthlySelector

--- frontend/src/components/forms/FormUserAccess/MonthlySelector.tsx
+++ frontend/src/components/forms/FormUserAccess/MonthlySelector.tsx
@@ -3,57 +3,84 @@ import { DayPicker } from "react-day-picker";
 import { es } from "react-day-picker/locale";
 import { CameraRegister } from "../../cameras/CameraRegister";
 import type { FormValues } from "../../../schemas/schemaForm";
+import type { UserWithoutDescriptor } from "../../../types/userType";
+import { useUpdateUser } from "../../../hooks/useUpdateUser";
 
-interface props{
-    backToOptions:()=>void
-    data:FormValues
-    initialValues?: string[]
+
+interface props {
+    backToOptions: () => void
+    data: FormValues
+    initialValues?: UserWithoutDescriptor
+    mode: "create" | "edit"
 }
 
-export const MonthlySelector=({initialValues,backToOptions,data}:props)=>{
 
-    const [selectedDays, setSelectedDays] = useState<Date[]>(initialValues?.map(d => new Date(d)) ?? []);
-    const [showCameraRegister,setShowCameraRegister]=useState(false);
+export const MonthlySelector = ({ initialValues, backToOptions, data, mode }: props) => {
 
-    //Agregamos a date los dias que selecciono el usuario (convertimos selectDays en un array de string) 
-    if(showCameraRegister)return <CameraRegister data={{...data,allowedDates:selectedDays.map(d => d.toISOString())}} backToForm={()=>setShowCameraRegister(false)}/>
 
-    return(
-        
-            <div className="flex flex-col items-center justify-around gap-10 bg-white h-screen">
-                {/* Titulo */}
-                <h1 className="text-3xl font-medium">Calendario</h1>
-                
-                {/* Calendario */}
-                <DayPicker mode="multiple" selected={selectedDays} onSelect={(days) => setSelectedDays(days ?? [])} required locale={es}
-                    classNames={{
-                        root: "h-120 w-full p-5 bg-white border-t border-b border-gray-400 shadow-[0_4px_10px_rgba(0,0,0,0.15),0_-4px_10px_rgba(0,0,0,0.15)]",
-                        month: "relative space-y-15",
-                        caption: "grid grid-cols-3 items-center px-5",
-                        caption_label: "text-xl font-bold m-22",
-                        nav: "flex items-center justify-between col-span-3",
-                        nav_button_previous: "pointer-events-auto justify-self-start text-gray-600",
-                        nav_button_next: "pointer-events-auto justify-self-end text-gray-600",
-                        weekdays: "grid grid-cols-7 text-xl font-medium text-gray-500",
-                        week: "grid grid-cols-7",
-                        day: "text-xl w-11 h-11 flex items-center justify-center rounded-lg m-0.5",
-                        selected: "bg-black text-white",
-                        day_today: "border border-gray-200",
-                        day_outside: "text-gray-300"}}/>
+    const [selectedDays, setSelectedDays] = useState<Date[]>(initialValues?.allowedDates?.map(d => new Date(d)) ?? []);
+    const [showCameraRegister, setShowCameraRegister] = useState(false);
+    const { userUpdate } = useUpdateUser()//Luego tenemos que traer los otros estados
 
-                {/* Botones */}
-                <div className="flex flex-row-reverse gap-20">
-                    <button
-                        onClick={()=>setShowCameraRegister(true)}//Abrimos el componente CameraRegister
-                        className="bg-black w-34 h-11 text-white rounded-lg shadow-lg transition-all duration-200
+
+    //Agregamos a date los dias que selecciono el usuario (convertimos selectDays en un array de string)
+    if (showCameraRegister) return <CameraRegister data={{ ...data, allowedDates: selectedDays.map(d => d.toISOString()) }} backToForm={() => setShowCameraRegister(false)} />
+
+
+    const handleConfirm = () => {
+        if (mode === "edit") {
+            if (!initialValues) return
+            userUpdate(initialValues._id, data)
+        } else {
+            setShowCameraRegister(true)
+        }
+    }
+
+
+    return (
+
+
+        <div className="flex flex-col items-center justify-around gap-10 bg-white h-screen">
+            {/* Titulo */}
+            <h1 className="text-3xl font-medium">Calendario</h1>
+
+
+            {/* Calendario */}
+            <DayPicker mode="multiple" selected={selectedDays} onSelect={(days) => setSelectedDays(days ?? [])} required locale={es}
+                classNames={{
+                    root: "h-120 w-full p-5 bg-white border-t border-b border-gray-400 shadow-[0_4px_10px_rgba(0,0,0,0.15),0_-4px_10px_rgba(0,0,0,0.15)]",
+                    month: "relative space-y-15",
+                    caption: "grid grid-cols-3 items-center px-5",
+                    caption_label: "text-xl font-bold m-22",
+                    nav: "flex items-center justify-between col-span-3",
+                    nav_button_previous: "pointer-events-auto justify-self-start text-gray-600",
+                    nav_button_next: "pointer-events-auto justify-self-end text-gray-600",
+                    weekdays: "grid grid-cols-7 text-xl font-medium text-gray-500",
+                    week: "grid grid-cols-7",
+                    day: "text-xl w-11 h-11 flex items-center justify-center rounded-lg m-0.5",
+                    selected: "bg-black text-white",
+                    day_today: "border border-gray-200",
+                    day_outside: "text-gray-300"
+                }} />
+
+
+
+
+            {/* Botones */}
+            <div className="flex flex-row-reverse gap-20">
+                <button
+                    onClick={handleConfirm}//Abrimos el componente CameraRegister
+                    className="bg-black w-34 h-11 text-white rounded-lg shadow-lg transition-all duration-200
                             active:bg-gray-200 active:shadow-inner">Siguiente</button>
-                    <button
-                    onClick={backToOptions} 
+                <button
+                    onClick={backToOptions}
                     className="bg-white border border-black/20 w-28 h-11 text-black rounded-lg shadow-lg transition-all duration-200
                                 active:bg-gray-200 active:shadow-inner">Volver</button>
-                </div>
-            </div>          
-        
+            </div>
+        </div>
+
+
+
 
     )
 }
--- frontend/src/components/forms/FormUserAccess/index.tsx
+++ frontend/src/components/forms/FormUserAccess/index.tsx
@@ -4,58 +4,58 @@ import { useState } from "react"
 import type { FormValues } from "../../../schemas/schemaForm"
 import type { UserWithoutDescriptor } from "../../../types/userType"
 
-interface props{
-    backToForm:()=>void
-    data:FormValues
+interface props {
+    backToForm: () => void
+    data: FormValues
     initialValue?: UserWithoutDescriptor
     //Tendria que recibir directamente el "mode" quizas, mas efectivo
 }
 
-export const FormUserAccess=({initialValue,backToForm,data}:props)=>{
+export const FormUserAccess = ({ initialValue, backToForm, data }: props) => {
 
-    const [option,setOption]=useState<"semanal"|"calendario"|null>(null)
+    const [option, setOption] = useState<"semanal" | "calendario" | null>(null)
 
     //En caso de elegir semanal, añadimos el valor del atributo en data
-    if (option === "semanal") return <WeeklySelector data={{...data,accessType:"semanal"}} backToOptions={()=>setOption(null)} initialValues={initialValue} mode={initialValue ? "edit" : "create"}/>
+    if (option === "semanal") return <WeeklySelector data={{ ...data, accessType: "semanal" }} backToOptions={() => setOption(null)} initialValues={initialValue} mode={initialValue ? "edit" : "create"} />
     //En caso de elegir mensual, añadimos el valor del atributo en data
-    if (option === "calendario") return <MonthlySelector data={{...data,accessType:"calendario"}} backToOptions={()=>setOption(null)} initialValues={initialValue?.allowedDates} />;
+    if (option === "calendario") return <MonthlySelector data={{ ...data, accessType: "calendario" }} backToOptions={() => setOption(null)} initialValues={initialValue} mode={initialValue ? "edit" : "create"} />;
 
-    return(
-            <div className="flex flex-col items-center justify-around gap-10 bg-white h-screen">
-            
-                {/* Titulo */}
-                <div>
-                    <h1 className="text-3xl font-medium">Acceso del visitante</h1>
-                </div>
+    return (
+        <div className="flex flex-col items-center justify-around gap-10 bg-white h-screen">
 
-                {/* Botones de mensual y semanal */}
-                <div className="flex flex-col justify-center items-center gap-15 
+            {/* Titulo */}
+            <div>
+                <h1 className="text-3xl font-medium">Acceso del visitante</h1>
+            </div>
+
+            {/* Botones de mensual y semanal */}
+            <div className="flex flex-col justify-center items-center gap-15 
                     shadow-[0_4px_10px_rgba(0,0,0,0.15),0_-4px_10px_rgba(0,0,0,0.15)] w-full p-10">
 
-                    <button
-                    onClick={()=>setOption("semanal")} 
+                <button
+                    onClick={() => setOption("semanal")}
                     className={`w-34 h-11 rounded-lg shadow-lg transition-all duration-200
-                        ${initialValue?.accessType==="semanal"
+                        ${initialValue?.accessType === "semanal"
                             ? "bg-white text-black border border-black/20"
                             : "bg-black text-white"
                         } active:bg-gray-200 active:shadow-inner`}>Semanal</button>
 
-                    <button 
-                    onClick={()=>setOption("calendario")}
+                <button
+                    onClick={() => setOption("calendario")}
                     className={`w-34 h-11 rounded-lg shadow-lg transition-all duration-200
-                        ${initialValue?.accessType==="calendario"
+                        ${initialValue?.accessType === "calendario"
                             ? "bg-white text-black border border-black/20"
                             : "bg-black text-white"
                         } active:bg-gray-200 active:shadow-inner`}>Calendario</button>
-                </div>
+            </div>
 
-                {/* Boton volver */}
-                <div className="flex flex-row-reverse gap-20">
-                    <button
-                        onClick={backToForm}
-                        className="bg-white border border-black/20 w-28 h-11 text-black rounded-lg shadow-lg transition-all duration-200
+            {/* Boton volver */}
+            <div className="flex flex-row-reverse gap-20">
+                <button
+                    onClick={backToForm}
+                    className="bg-white border border-black/20 w-28 h-11 text-black rounded-lg shadow-lg transition-all duration-200
                             active:bg-gray-200 active:shadow-inner">Volver</button>
-                </div>
-            </div>    
+            </div>
+        </div>
     )
 }
