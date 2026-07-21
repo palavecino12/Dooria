0f0d933cc4829a528b6e26f0d1877ee8687e69b0
Facundo <facundopalavecino054@gmail.com>
2026-07-21 15:35:06 -0300

    identificacion de quien es intercom y quien quiere verlo mediante socket

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -2,15 +2,41 @@ import { Server, Socket } from "socket.io";
 
 export const configureSockets = (io: Server) => {
 
+    let intercom: Socket | null = null;
+
     //Obtenemos el objeto que reprensenta la conexion de un cliente cuando entra.
     io.on("connection", (socket: Socket) => {
 
-        console.log(`Cliente conectado: ${socket.id}`);
+        //Identificamos quein funciona como intercom
+        socket.on("join-intercom", () => {
 
+            intercom = socket;
+
+            console.log("Intercom conectado");
+        });
+
+        //Identificamos quien quiere ver el intercom
+        socket.on("watch-intercom", () => {
+
+            console.log(`Mobile ${socket.id} quiere ver el intercom`);
+
+            if (intercom) {
+                intercom.emit("viewer-connected", {
+                    viewerId: socket.id
+                });
+            }
+
+        });
+
+        //Si el que hacia de intercom se sale, dejamos el intercom libre
         socket.on("disconnect", () => {
-            console.log(`Cliente desconectado: ${socket.id}`);
+            if (socket === intercom) {
+                intercom = null;
+            }
         });
 
     });
+
+
 
 };
